a1f06cf5990c94709f5783e4c83d90a14217ff0a
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-02 00:24:05 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,51 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
+  
+  
+  def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
+
+def turn_count(board)
+ counter = 0
+ board.each do |choices|
+  if choices == "X" || choices == "O"
+    counter +=1 
+end
+end
+  counter
+end
+
+def current_player(board)
+  turn_count % 2 = 0 ? "X" : "O"
+end
